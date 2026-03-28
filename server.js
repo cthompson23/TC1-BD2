4880de093508d4c3199c4a7e3c3bebe4c64a8457
Camila Thompson <132382417+cthompson23@users.noreply.github.com>
2026-03-27 21:32:48 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,41 +1,7 @@
-const express = require("express");
-const session = require("express-session");
-const { keycloak, memoryStore } = require("./src/config/keycloak.js");
-
-const restaurant_routes = require("./src/routes/restaurant_routes.js");
-const menu_routes = require("./src/routes/menus_routes.js");
-const dishes_routes = require("./src/routes/dishes_routes.js");
-const tables_routes = require("./src/routes/tables_routes.js");
-const error_handler = require("./src/middleware/error_handler.js");
-
 require("dotenv").config();
 
-const app = express();
-const port = process.env.PORT || 5000;
-
-app.use(express.json()); 
-
-
-app.use(
-  session({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: true,
-    store: memoryStore
-  })
-);
-
-app.use(keycloak.middleware());
-
-//ROUTES
-app.use("/api", restaurant_routes);
-app.use("/api", menu_routes);
-app.use("/api", dishes_routes);
-app.use("/api", tables_routes);
-
-
-// middleware error handler
-app.use(error_handler);
+const app = require("./src/app.js");
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
